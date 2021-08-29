9570a277a52359acbdf62c0c9cf674973e74f174
Micheal Limones <Micheal.Limones@du.edu>
2021-08-28 20:38:34 -0600

    added team member questions.

--- index.js
+++ index.js
@@ -45,7 +45,59 @@ const promptManagerQuestions = () => {
 };
 
 const addTeamMembers = () => {
-  return;
+  return inquirer.prompt([
+    {
+      type: 'list',
+      message: 'Would you like to add an employee?',
+      choices: ['Yes', 'No'],
+      name: 'addEmployee',
+    },
+    {
+      type: 'list',
+      message: "PLease select the employee's role.",
+      choices: ['Manager', 'Engineer', 'Intern'],
+      name: 'roles',
+    },
+    {
+      type: 'input',
+      message: "Please enter the employee's name.",
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message: "Please enter the employee's ID number.",
+      name: 'id',
+    },
+    {
+      type: 'input',
+      message: "Please enter the employee's email address.",
+      name: 'email',
+    },
+    {
+      when: (position) => {
+        return position.role === 'Manager';
+      },
+      type: 'input',
+      message: "Please enter the employee's office phone number.",
+      name: 'officeNumber',
+    },
+    {
+      when: (position) => {
+        return position.role === 'Engineer';
+      },
+      type: 'input',
+      message: "Please enter the employee's Github username.",
+      name: github,
+    },
+    {
+      when: (position) => {
+        return position.role === 'Intern';
+      },
+      type: 'input',
+      message: "Please enter the employee's school name.",
+      name: 'school',
+    },
+  ]);
 };
 
 const init = () => {
