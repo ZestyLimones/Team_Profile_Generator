a3769139bee791a01c7bce990a9291b1808e0fa7
Micheal Limones <Micheal.Limones@du.edu>
2021-08-28 19:36:48 -0600

    added manager questions function.

--- index.js
+++ index.js
@@ -5,3 +5,53 @@ const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+const team = [];
+
+const promptManagerQuestions = () => {
+  return inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: 'Please enter your name.',
+        name: 'name',
+      },
+      {
+        type: 'input',
+        message: 'Please enter your ID number.',
+        name: 'id',
+      },
+      {
+        type: 'input',
+        message: 'Please enter your email address.',
+        name: 'email',
+      },
+      {
+        type: 'input',
+        message: 'Please enter your office phone number',
+        name: 'officeNumber',
+      },
+    ])
+    .then((responses) => {
+      const manager = new Manager(
+        responses.name,
+        responses.id,
+        responses.email,
+        responses.officeNumber
+      );
+      team.push(manager);
+      addTeamMembers();
+    });
+};
+
+const addTeamMembers = () => {
+  return;
+};
+
+const init = () => {
+  promptManagerQuestions().then((responses) => {
+    console.log(responses);
+  });
+};
+
+init();
--- test/Employee.test.js
+++ test/Employee.test.js
@@ -3,5 +3,6 @@ const Employee = require('../lib/Employee');
 describe('employee', () => {
   it('if entered correctly, get an employee', () => {
     const john = new Employee('John', '96312', 'jsmith@mycompanyemail.com');
-    expect(john).toEqual(true);
+    expect(john).toBe('John', '96312', 'jsmith@mycompanyemail.com');
+  });
 });
