d63d983859278490825542321ce38d4273720ffb
Micheal Limones <Micheal.Limones@du.edu>
2021-08-29 17:22:24 -0600

    added functions to get employee prompts to build team array.

--- index.js
+++ index.js
@@ -45,64 +45,121 @@ const promptManagerQuestions = () => {
 };
 
 const addTeamMembers = () => {
-  return inquirer.prompt([
-    {
-      type: 'list',
-      message: 'Would you like to add an employee?',
-      choices: ['Yes', 'No'],
-      name: 'addEmployee',
-    },
-    {
-      type: 'list',
-      message: "PLease select the employee's role.",
-      choices: ['Manager', 'Engineer', 'Intern'],
-      name: 'roles',
-    },
-    {
-      type: 'input',
-      message: "Please enter the employee's name.",
-      name: 'name',
-    },
-    {
-      type: 'input',
-      message: "Please enter the employee's ID number.",
-      name: 'id',
-    },
-    {
-      type: 'input',
-      message: "Please enter the employee's email address.",
-      name: 'email',
-    },
-    {
-      when: (position) => {
-        return position.role === 'Manager';
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: 'Would you like to add an employee?',
+        choices: ['Yes', 'No'],
+        name: 'addMember',
       },
-      type: 'input',
-      message: "Please enter the employee's office phone number.",
-      name: 'officeNumber',
-    },
-    {
-      when: (position) => {
-        return position.role === 'Engineer';
-      },
-      type: 'input',
-      message: "Please enter the employee's Github username.",
-      name: 'github',
-    },
-    {
-      when: (position) => {
-        return position.role === 'Intern';
-      },
-      type: 'input',
-      message: "Please enter the employee's school name.",
-      name: 'school',
-    },
-  ]);
+    ])
+    .then((responses) => {
+      if (responses.addMember === 'Yes') {
+        promptTeamQuestions();
+      } else {
+        // generageHTML();
+        console.log(team);
+      }
+    });
 };
+
+const promptTeamQuestions = () => {
+  return inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: "PLease select the employee's role.",
+        choices: ['Manager', 'Engineer', 'Intern'],
+        name: 'roles',
+      },
+      {
+        type: 'input',
+        message: "Please enter the employee's name.",
+        name: 'name',
+      },
+      {
+        type: 'input',
+        message: "Please enter the employee's ID number.",
+        name: 'id',
+      },
+      {
+        type: 'input',
+        message: "Please enter the employee's email address.",
+        name: 'email',
+      },
+      {
+        when: (responses) => {
+          return responses.roles === 'Manager';
+        },
+        type: 'input',
+        message: "Please enter the employee's office phone number.",
+        name: 'officeNumber',
+      },
+      {
+        when: (responses) => {
+          return responses.roles === 'Engineer';
+        },
+        type: 'input',
+        message: "Please enter the employee's Github username.",
+        name: 'github',
+      },
+      {
+        when: (responses) => {
+          return responses.roles === 'Intern';
+        },
+        type: 'input',
+        message: "Please enter the employee's school name.",
+        name: 'school',
+      },
+    ])
+    .then((responses) => {
+      createTeam(responses);
+      addTeamMembers();
+    });
+};
+
+const createTeam = (responses) => {
+  if (responses.roles === 'Manager') {
+    let addEmployee = new Manager(
+      responses.name,
+      responses.id,
+      responses.email,
+      responses.officeNumber
+    );
+    team.push(addEmployee);
+  } else if (responses.roles === 'Engineer') {
+    let addEmployee = new Engineer(
+      responses.name,
+      responses.id,
+      responses.email,
+      responses.github
+    );
+    team.push(addEmployee);
+  } else if (responses.roles === 'Intern') {
+    let addEmployee = new Intern(
+      responses.name,
+      responses.id,
+      responses.email,
+      responses.school
+    );
+    team.push(addEmployee);
+  }
+};
+
+// const generageHTML = () => {
+//   fs.writeFileSync()
+// }
 
 const init = () => {
   promptManagerQuestions().then((responses) => {
-    console.log(responses);
+    // const manager = new Manager(
+    //   responses.name,
+    //   responses.id,
+    //   responses.email,
+    //   responses.officeNumber
+    // );
+    // team.push(manager);
   });
 };
 
